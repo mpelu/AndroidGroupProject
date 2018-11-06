e20dc1f98dbe093b61831b12d4f5805ba436190b
Luke <gama0028@algonquinlive.com>
2018-11-06 10:07:29 -0500

    initial commit

---
+++ app/src/main/java/com/example/mpelu/androidgroupproject/OcTranspo.java
@@ -0,0 +1,13 @@
+package com.example.mpelu.androidgroupproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OcTranspo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oc_transpo);
+    }
+}
