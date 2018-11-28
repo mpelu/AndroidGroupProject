59276e775ebaba58997a8b978521d5650c91d3c7
Patty <pmrm1984@gmail.com>
2018-11-27 20:13:21 -0500

    test

--- app/src/main/java/com/example/mpelu/androidgroupproject/MainActivity.java
+++ app/src/main/java/com/example/mpelu/androidgroupproject/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.mpelu.androidgroupproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+    Button nutritionBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nutritionBtn = findViewById(R.id.nutritionInfoBtn);
+
+        nutritionBtn.setOnClickListener( e -> {
+            Intent intent = new Intent(MainActivity.this, NutritionDatabase.class);
+            startActivity(intent);
+        });
     }
 }
